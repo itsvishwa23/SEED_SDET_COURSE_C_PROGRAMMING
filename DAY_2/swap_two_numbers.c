f887a0b4ff6a5aafdccd8b8441fadc3898e2c9c1
itsvishwa23 <itsvishwa23@gmail.com>
2024-02-28 17:28:52 +0530

    Day 2

--- DAY_2/swap_two_numbers.c
+++ DAY_2/swap_two_numbers.c
@@ -1,2 +1,21 @@
 #include<stdio.h>
-sdf
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    printf("Enter the First Number");
+    scanf("%d"&a);
+
+    printf("Enter the Second Number");
+    scanf("%d"&b);
+
+    a=b;
+    b=a;
+    a=c;
+
+    printf("The numbers after swapping are\n\n");
+    printf("Number 1= %d Number 2= %d");
+
+}
